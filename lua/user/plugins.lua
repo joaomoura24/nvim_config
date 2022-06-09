b2eb14c243a2e8ec2801f72d18561071581ca044
Joao Moura (ATI office) <joaopousa24@gmail.com>
2022-06-09 17:50:05 +0100

    Add completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,15 +62,14 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/nvim-cmp"          -- The completion plugin
+  use "hrsh7th/cmp-buffer"        -- buffer completions
+  use "hrsh7th/cmp-path"          -- path completions
+  use "hrsh7th/cmp-cmdline"       -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"  -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
   -- snippets
