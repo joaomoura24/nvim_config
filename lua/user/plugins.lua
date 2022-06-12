dac2f7126e938427998850fea6c7d5cbd05865b0
Joao Moura (ATI office) <joaopousa24@gmail.com>
2022-06-12 10:48:54 +0100

    Add illuminate plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,8 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
 
+  use "lambdalisue/nerdfont.vim"      -- for handling nerd fonts
+
   use "tpope/vim-characterize"  -- for revealing character representation with ga
 
   -- Colorschemes
@@ -69,21 +71,22 @@ return packer.startup(function(use)
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp"          -- The completion plugin
+  use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-buffer"        -- buffer completions
   use "hrsh7th/cmp-path"          -- path completions
   use "hrsh7th/cmp-cmdline"       -- cmdline completions
   use "saadparwaiz1/cmp_luasnip"  -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
+  use "neovim/nvim-lspconfig"           -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "tamago324/nlsp-settings.nvim"    -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "RRethy/vim-illuminate"           -- for highlighting keywords
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
